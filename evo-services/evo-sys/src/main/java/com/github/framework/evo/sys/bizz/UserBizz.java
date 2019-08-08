3ac6a323adaf6d20776a35e532aed353b8372932
kyllyang <kyllyang@gmail.com>
2019-08-08 15:41:13 +0800

    测试密码修改方法

--- evo-services/evo-sys/src/main/java/com/github/framework/evo/sys/bizz/UserBizz.java
+++ evo-services/evo-sys/src/main/java/com/github/framework/evo/sys/bizz/UserBizz.java
@@ -105,6 +105,8 @@ public class UserBizz extends BaseXmlBizz<UserDao, User, Long, UserDto> {
 	@Transactional
 	public void updatePassowrd(UserDto dto) {
 		UserDto oldUserDto = this.get(dto.getId());
+		log.info("old password: " + oldUserDto.getPassword());
+		log.info("new password: " + dto.getPassword());
 		oldUserDto.setPassword(passwordEncoder.encode(dto.getPassword()));
 		this.update(oldUserDto);
 	}
