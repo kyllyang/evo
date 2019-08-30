d7d5ce8fa1df8eff4fc3841fd89a69501a9dc934
kyllyang <kyllyang@gmail.com>
2019-08-30 11:26:50 +0800

    修改日期工具类

--- evo-components/evo-common/src/main/java/com/github/framework/evo/common/uitl/DateUtil.java
+++ evo-components/evo-common/src/main/java/com/github/framework/evo/common/uitl/DateUtil.java
@@ -1,11 +1,12 @@
 package com.github.framework.evo.common.uitl;
 
 import lombok.extern.slf4j.Slf4j;
-import org.joda.time.DateTime;
-import org.joda.time.LocalTime;
 
+import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
@@ -61,29 +62,148 @@ public class DateUtil {
 		return date == null ? null : DateTimeFormatter.ofPattern(pattern).format(LocalDateTime.ofInstant(Instant.ofEpochMilli(date.getTime()), ZoneId.systemDefault()));
 	}
 
-	public static Date toDate(long millis) {
-		return new DateTime(millis).toDate();
-	}
-
 	public static Date removeHMS(Date date) {
 		return parseDate(formatDate(date));
 	}
 
-	public static Date now() {
-		return DateTime.now().toDate();
+	public static Date plusNanos(Date date, long nanos) {
+		return toDate(toDateTime(date).plusNanos(nanos));
 	}
 
-	public static Date plusMinutes(Date date, int minutes){
-		return new DateTime(date).plusMinutes(minutes).toDate();
+	public static Date plusSeconds(Date date, long seconds) {
+		return toDate(toDateTime(date).plusSeconds(seconds));
+	}
+
+	public static Date plusMinutes(Date date, long minutes) {
+		return toDate(toDateTime(date).plusMinutes(minutes));
+	}
+
+	public static Date plusHours(Date date, long hours) {
+		return toDate(toDateTime(date).plusHours(hours));
+	}
+
+	public static Date plusDays(Date date, long days) {
+		return toDate(toDateTime(date).plusDays(days));
+	}
+
+	public static Date plusWeeks(Date date, long weeks) {
+		return toDate(toDateTime(date).plusWeeks(weeks));
+	}
+
+	public static Date plusMonths(Date date, long months) {
+		return toDate(toDateTime(date).plusMonths(months));
+	}
+
+	public static Date plusYears(Date date, long years) {
+		return toDate(toDateTime(date).plusYears(years));
+	}
+
+	public static Date minusNanos(Date date, long nanos) {
+		return toDate(toDateTime(date).minusNanos(nanos));
+	}
+
+	public static Date minusSeconds(Date date, long seconds) {
+		return toDate(toDateTime(date).minusSeconds(seconds));
+	}
+
+	public static Date minusMinutes(Date date, long minutes) {
+		return toDate(toDateTime(date).minusMinutes(minutes));
+	}
+
+	public static Date minusHours(Date date, long hours) {
+		return toDate(toDateTime(date).minusHours(hours));
+	}
+
+	public static Date minusDays(Date date, long days) {
+		return toDate(toDateTime(date).minusDays(days));
+	}
+
+	public static Date minusWeeks(Date date, long weeks) {
+		return toDate(toDateTime(date).minusWeeks(weeks));
+	}
+
+	public static Date minusMonths(Date date, long months) {
+		return toDate(toDateTime(date).minusMonths(months));
+	}
+
+	public static Date minusYears(Date date, long years) {
+		return toDate(toDateTime(date).minusYears(years));
+	}
+
+	public static Date now() {
+		return Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant());
+	}
+
+	public static LocalDate nowDate() {
+		return LocalDate.now();
+	}
+
+	public static LocalDateTime nowDateTime() {
+		return LocalDateTime.now();
+	}
+
+	public static LocalTime nowTime() {
+		return LocalTime.now();
+	}
+
+	public static LocalDate toDate(long millis) {
+		return Instant.ofEpochMilli(millis).atZone(ZoneId.systemDefault()).toLocalDate();
+	}
+
+	public static LocalDateTime toDateTime(long millis) {
+		return Instant.ofEpochMilli(millis).atZone(ZoneId.systemDefault()).toLocalDateTime();
+	}
+
+	public static LocalTime toTime(long millis) {
+		return Instant.ofEpochMilli(millis).atZone(ZoneId.systemDefault()).toLocalTime();
+	}
+
+	public static Date toDate(LocalDate localDate) {
+		return Date.from(localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+	}
+
+	public static Date toDate(LocalDateTime localDateTime) {
+		return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+	}
+
+	public static Date toDate(LocalTime localTime) {
+		return Date.from(localTime.atDate(LocalDate.of(1970, 1, 1)).atZone(ZoneId.systemDefault()).toInstant());
+	}
+
+	public static LocalDate toDate(Date date) {
+		return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDate();
+	}
+
+	public static LocalDateTime toDateTime(Date date) {
+		return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDateTime();
+	}
+
+	public static LocalTime toTime(Date date) {
+		return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalTime();
 	}
 
 	public static boolean inRangeTime(String time, String startTime, String endTime) {
-		String[] ts = time.split(":");
-		String[] sts = startTime.split(":");
-		String[] ets = endTime.split(":");
-		LocalTime lt = new LocalTime(Integer.parseInt(ts[0]), Integer.parseInt(ts[1]), 0, 0);
-		LocalTime slt = new LocalTime(Integer.parseInt(sts[0]), Integer.parseInt(sts[1]), 0, 0);
-		LocalTime elt = new LocalTime(Integer.parseInt(ets[0]), Integer.parseInt(ets[1]), 0, 0);
-		return lt.isAfter(slt) && lt.isBefore(elt);
+		LocalTime lt = LocalTime.parse(time);
+		return lt.isAfter(LocalTime.parse(startTime)) && lt.isBefore(LocalTime.parse(endTime));
+	}
+
+	public static long intervalNanos(String startTime, String endTime) {
+		return Duration.between(LocalTime.parse(startTime), LocalTime.parse(endTime)).toNanos();
+	}
+
+	public static long intervalMillis(String startTime, String endTime) {
+		return Duration.between(LocalTime.parse(startTime), LocalTime.parse(endTime)).toMillis();
+	}
+
+	public static long intervalMinutes(String startTime, String endTime) {
+		return Duration.between(LocalTime.parse(startTime), LocalTime.parse(endTime)).toMinutes();
+	}
+
+	public static long intervalHours(String startTime, String endTime) {
+		return Duration.between(LocalTime.parse(startTime), LocalTime.parse(endTime)).toHours();
+	}
+
+	public static long intervalDays(String startTime, String endTime) {
+		return Duration.between(LocalTime.parse(startTime), LocalTime.parse(endTime)).toDays();
 	}
 }
