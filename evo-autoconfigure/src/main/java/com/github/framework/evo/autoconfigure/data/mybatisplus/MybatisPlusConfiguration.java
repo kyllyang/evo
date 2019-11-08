300c1aa2b70c1a47a6e1341a818b53d0f5488b71
kyllyang <kyllyang@gmail.com>
2019-11-08 13:42:38 +0800

    调整mybatis-plus分页限制

--- evo-autoconfigure/src/main/java/com/github/framework/evo/autoconfigure/data/mybatisplus/MybatisPlusConfiguration.java
+++ evo-autoconfigure/src/main/java/com/github/framework/evo/autoconfigure/data/mybatisplus/MybatisPlusConfiguration.java
@@ -15,6 +15,8 @@ import org.springframework.context.annotation.Configuration;
 public class MybatisPlusConfiguration {
 	@Bean
 	public PaginationInterceptor paginationInterceptor() {
-		return new PaginationInterceptor();
+		PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+		paginationInterceptor.setLimit(Integer.MAX_VALUE);// 默认每页限制500条记录，这里使用整型最大值，相当于没有限制了
+		return paginationInterceptor;
 	}
 }
