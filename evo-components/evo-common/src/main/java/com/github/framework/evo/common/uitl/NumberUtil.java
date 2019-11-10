2cadf2ace966b72995ad58f61ba8422b59c39270
kyllyang <kyllyang@gmail.com>
2019-11-10 16:59:31 +0800

    增加四舍五入方法

--- evo-components/evo-common/src/main/java/com/github/framework/evo/common/uitl/NumberUtil.java
+++ evo-components/evo-common/src/main/java/com/github/framework/evo/common/uitl/NumberUtil.java
@@ -1,5 +1,8 @@
 package com.github.framework.evo.common.uitl;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * User: Kyll
  * Date: 2019-11-10 11:46
@@ -11,5 +14,9 @@ public class NumberUtil {
 		}
 
 		return Integer.parseInt(str.replaceAll("^(0+)", ""));
+	}
+
+	public static double round(double value, int scale) {
+		return new BigDecimal(value).setScale(scale, RoundingMode.HALF_UP).doubleValue();
 	}
 }
